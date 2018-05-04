4b453ee1398b5bdcfef31f5026dc8521b2b7d9c0
CecrobienR <tom.hattermann@gmail.com>
2018-05-04 16:02:56 -0300

    lolikroo

--- R/Double.R
+++ R/Double.R
@@ -14,3 +14,48 @@ Double <- function(number) {
   return(2L * number)
 }
 
+
+
+
+#' fuzzydouble
+#'
+#' @param x
+#' @param mean
+#' @param sd
+#'
+#' @return fuzzy multiplication
+#' @export
+#'
+#' @examples
+fuzzydouble <- function(x, mean = 0, sd = 1) {
+
+
+
+  y <- 2 * x + stats::rnorm(n = length(x), mean = mean, sd = sd)
+
+  fuzzydouble <- data.frame(x = x, y = y)
+
+  class(fuzzydouble)<- c("fuzzydouble", class(fuzzydouble))
+  return(fuzzydouble)
+}
+
+
+
+#' plot.FuzzyDouble
+#'
+#' @param x
+#' @param xlab
+#' @param ylab
+#' @param ...
+#' @param LineCol
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.FuzzyDouble <-function(x, xlab = "x", ylab = "Double",..., LineCol = "red") {
+  # xy standard plot
+  graphics::plot(x$x, x$y, xlab = xlab, ylab = ylab,...)
+  # Add the regression line
+  graphics::lines(x$x, 2*x$x, col = LineCol)
+}
